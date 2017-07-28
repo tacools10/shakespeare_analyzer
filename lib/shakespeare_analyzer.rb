936c7615c0c607ca6936545b76344a22cd500090
Thomas Cools <tacools10@gmail.com>
2017-07-28 17:39:37 -0400

    Trying to test using Rspec

--- lib/shakespeare_analyzer.rb
+++ lib/shakespeare_analyzer.rb
@@ -2,18 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 
 class ShakespeareAnalyzer
-  # Your code goes here...
 
   def initialize(url)
     @url = url
     @totals = Hash.new
     @speakers = Array.new
+    @xml_doc = Nokogiri::XML(open(url))
   end
 
   attr_reader :url, :totals, :speakers
+  attr_accessor :xml_doc
 
-  def get_speakers
-     xml_doc = Nokogiri::XML(open(url))
+  def get_speakers(xml_doc)
      xml_doc.xpath("//SPEECH/SPEAKER").each do |speaker|
        if @speakers.length == 0 || !@speakers.include?("#{speaker.text}")
           @speakers << speaker.text
@@ -22,14 +22,15 @@ class ShakespeareAnalyzer
      return @speakers
   end
 
-  def parse_count_sort(speakers)
-    xml_doc = Nokogiri::XML(open(url))
+  def parse_count_sort(speakers, xml_doc)
     speakers.each do |character|
       count = 0
       xml_doc.xpath("//SPEECH[SPEAKER[text() = '#{character}']]/LINE").each do |line|
           count += 1
       end
-      @totals["#{character.upcase}"] = count
+      if character != "ALL"
+        @totals["#{character.upcase}"] = count
+      end
     end
 
     return @totals.sort_by { |char, count| count}.reverse
@@ -39,7 +40,5 @@ end
 
 example = ShakespeareAnalyzer.new("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml")
 
-values = example.parse_count_sort(example.get_speakers)
+values = example.parse_count_sort(example.get_speakers(example.xml_doc), example.xml_doc)
 
-
-# //SPEECH[SPEAKER[text() = '#{character.upcase}']]/LINE
--- spec/shakespeare_analyzer_spec.rb
+++ spec/shakespeare_analyzer_spec.rb
@@ -1,11 +1,30 @@
 require "spec_helper"
+require 'shakespeare_analyzer'
+require 'nokogiri'
 
-describe ShakespeareAnalyzer do
-  it "has a version number" do
-    expect(ShakespeareAnalyzer::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+RSpec.describe ShakespeareAnalyzer do
+  context ".get_speakers"
+    it "takes all unique elements for a given node" do
+      test_analyzer = ShakespeareAnalyzer.new("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml")
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.root {
+          xml.SPEECH {
+            xml.SPEAKER "MACBETH"
+            xml.LINE "test line 1"
+            xml.LINE "test line 2"
+          }
+          xml.SPEECH {
+            xml.SPEAKER "DUNCAN"
+            xml.LINE "test line 3"
+            xml.LINE "test line 4"
+          }
+        }
+      end
+      test_xml = builder.to_xml
+      puts test_xml
+      test_analyzer.xml_doc = test_xml
+      puts test_analyzer.get_speakers(test_analyzer.xml_doc)
+      # expect(test_analyzer.get_speakers(test_analyzer.xml)).to eq ["MACBETH", "DUNCAN"]
+    end
 end
+
