a7e6143dc13d6c5870ce9e6deb1485222ad07314
Thomas Cools <tacools10@gmail.com>
2017-07-28 17:59:06 -0400

    Finished tests for both methods

--- spec/shakespeare_analyzer_spec.rb
+++ spec/shakespeare_analyzer_spec.rb
@@ -20,11 +20,30 @@ RSpec.describe ShakespeareAnalyzer do
           }
         }
       end
-      test_xml = builder.to_xml
-      puts test_xml
-      test_analyzer.xml_doc = test_xml
-      puts test_analyzer.get_speakers(test_analyzer.xml_doc)
-      # expect(test_analyzer.get_speakers(test_analyzer.xml)).to eq ["MACBETH", "DUNCAN"]
+      test_xml = Nokogiri::XML(builder.to_xml)
+      expect(test_analyzer.get_speakers(test_xml)).to eq ["MACBETH", "DUNCAN"]
+    end
+
+  context ".parse_count_sort"
+    it "returns a sorted hash of lines for each character" do
+      test_analyzer = ShakespeareAnalyzer.new("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml")
+      speakers = ["MACBETH", "DUNCAN"]
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.root {
+          xml.SPEECH {
+            xml.SPEAKER "MACBETH"
+            xml.LINE "test line 1"
+            xml.LINE "test line 2"
+          }
+          xml.SPEECH {
+            xml.SPEAKER "DUNCAN"
+            xml.LINE "test line 3"
+            xml.LINE "test line 4"
+          }
+        }
+      end
+      test_xml = Nokogiri::XML(builder.to_xml)
+      expect(test_analyzer.parse_count_sort(speakers, test_xml)).to eq [["DUNCAN", 2], ["MACBETH", 2]]
     end
 end
 
